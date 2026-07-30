aa473d3e84bf2ade24651d2f0f7c32caee4b6100
nothing-4413 <2010640834@qq.com>
2026-07-30 16:42:26 +0800

    模块 2：ACE_Reactor 版 Echo Server

--- server.cpp
+++ server.cpp
@@ -1,72 +1,146 @@
 #include<ace/INET_Addr.h>
 #include <ace/SOCK_Acceptor.h>
 #include <ace/SOCK_Stream.h>
+#include <ace/Event_Handler.h>
+#include <ace/Reactor.h>
 #include <ace/OS_NS_string.h>
 
 #include<iostream>
 
-int main()
+const unsigned short PORT = 9000;
+const int BUFFER_SIZE = 4096;
+
+class ClientHandler:public ACE_Event_Handler
 {
-    const unsigned short port = 9000;
-    const int buffer_size = 4096;
+    public:
+        explicit ClientHandler(ACE_SOCK_Stream stream):stream_(stream){}
 
-    ACE_INET_Addr listen_addr(port);
-
-    ACE_SOCK_Acceptor acceptor;
-
-    if(acceptor.open(listen_addr,1)==-1)
-    {
-        std::cerr<<"listen failed"<<std::endl;
-        return 1;
-    }
-    
-    std::cout<<"server listening on port"<<port<<std::endl;
-
-    while(true)
-    {
-        ACE_SOCK_Stream client_stream;
-        ACE_INET_Addr client_addr;
-        
-        std::cout<<"waiting for client..."<<std::endl;
-
-        if(acceptor.accept(client_stream,&client_addr)==-1)
+        ACE_HANDLE get_handle()const override
         {
-            std::cerr<<"accept falied"<<std::endl;
-            continue;
+            return stream_.get_handle();
         }
 
-        std::cout<<"client connected"<<std::endl;
-
-        char buffer[buffer_size];
-
-        while(true)
+        int handle_input(ACE_HANDLE fd)override
         {
+            char buffer[BUFFER_SIZE];
             ACE_OS::memset(buffer,0,sizeof(buffer));
 
-            ssize_t n = client_stream.recv(buffer,sizeof(buffer)-1);
+            ssize_t n = stream.recv(buffer,sizeof(buffer)-1);
             
             if(n>0)
             {
                 std::cout << "recevied:"<<buffer<<std::endl;
 
-                if(client_stream.send_n(buffer,n)!=n)
+                if(stream.send_n(buffer,n)!=n)
                 {
                     std::cerr<<"send failed"<<std::endl;
-                    break;
+                    return -1;
                 }
+                return 0;
             }
-            else if(n==0)
+            if(n==0)
             {
                 std::cout<<"client disconnected" <<std::endl;
-                break;
             }
             else
             {
                 std::cerr<"recv failed"<<std::endl;
-                break;
             }
+            return -1;
         }
-        clien_stream.close();
+
+        int handle_close(ACE_HANDLE handle,ACE_Reactor_Mask close_mask)override
+        {
+            std::cout<<"closing client handler"<<std::endl;
+
+            stream_.close()
+
+            delete this;
+            
+            return 0;
+        }
+    private:
+        ACE_SOCK_Stream stream_;
+};
+
+class AcceporHandler : public ACE_Event_Handler
+{
+    public:
+        explicit AcceptorHandler(unsigned short port):listen_addr_(port){}
+
+        int open()
+        {
+            if(acceptor.open(listen_addr_,1)==-1)
+            {
+                std::cerr<<"failed to listen"<<std::endl;
+                return 1;
+            }
+
+            std::cout<<"reactor echo server listening on port"<<PORT<<std::endl;
+            return 0;
+        }
+
+        ACE_HANDLE get_line()const override
+        {
+            return acceptor_.getline();
+        }
+
+        inr handle_input(ACE_HANDLE fd)override
+        {
+            ACE_SOCK_Stream client_stream;
+            ACE_INET_Addr client_addr;
+
+            if(acceptor_.accept(client_stream,&client_addr)==-1)
+            {
+                std::cerr<<"accept falied"<<std::endl;
+                return 0;
+            }
+
+            char addr_text[256]={0};
+            client_addr.addr_to_string(addr_text,sizeof(addr_text));
+
+            std::cout<<"client connected:"<<addr_text<<std::endl;
+
+            ClientHandler* client_handler = new ClientHandler(client_stream);
+
+            if(ACE_Reactor::instance()->register_handler(client_handler,ACE_Event_Handler::READ_MASK)==-1)
+            {
+                std::cerr<<"register client handler failed"<<std::endl;
+                client_stream.close();
+                delete client_handler;
+            }
+            return 0;
+        }
+        
+        int handle_close(ACE_HANDLE handle,ACE_Reactor_Mask close_mask)override
+        {
+            acceptor_.close();
+            return 0;
+        }
+    private:
+        ACE_INET_Addr listen_addr_;
+        ACE_SOCK_Acceptor acceptor_;
+};
+
+int main()
+{
+
+    AcceptorHandler acceptor_handler(PORT);
+
+    if(acceptor_handler.open()==-1)
+    {
+        return 1;
     }
+    
+    if(ACE_Reactor::instance()->register_handler(&acceptor_handler,ACE_Event_Handlerd::READ_MASK)==-1)
+    {
+        std::cerr<<"register acceptor handler failed"<<std::endl;
+        return 1;
+    }
+
+    std::cout<<"event loop started"<<std::endl;
+
+    ACE_Reactor::instance()->run_reactor_event_loop();
+    
     return 0;
 }
